ef3b5bf2bbacc96b201bc83f49f97b0506ee731f
Sugandhi13 <>
2023-07-25 13:16:24 +0000

    created functions for user input and updating student data in google sheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import json
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -10,12 +11,46 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('prepare-result')
+SHEET = GSPREAD_CLIENT.open('prepare_result')
 
-result = SHEET.worksheet('result')
 
-data = result.get_all_values()
+def student_data():
+    """
+    get Student details
+    """
+    data = []
+    std_name = input("Enter Student Full Name: \n")
+    data.append(std_name)
+    swedish_marks = input("Enter Marks in Swedish: \n")
+    data.append(swedish_marks)
+    english_marks = input("Enter Marks in English: \n")
+    data.append(english_marks)
+    mathematics_marks = input("Enter Marks in Mathematics: \n")
+    data.append(mathematics_marks)
+    science_marks = input("Enter Marks in Science: \n")
+    data.append(science_marks)
+    technology_marks = input("Enter Marks in Technology: \n")
+    data.append(technology_marks)
+    print(std_name, swedish_marks, english_marks, mathematics_marks, science_marks, technology_marks)
+    return data
 
-print(data)
 
-    
+stddata = student_data()
+
+
+def update_studentdata(new_row, worksheet):
+    """
+    Update the  worksheet,
+    data entered by teacher.
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+
+    # adds new row to the end of the current data
+    worksheet_to_update.append_row(new_row)
+
+    print(f"{worksheet} worksheet updated successfully\n")
+
+
+update_studentdata(stddata, "student_data")
+
